4d39591d8d6f30fc875f5f3ecc52857e43378be9
Princetech-bwb <princerwanda2003@gmail.com>
2025-06-19 07:01:07 +0300

    Update contact2.js

--- commandes/contact2.js
+++ commandes/contact2.js
@@ -1,3 +1,4 @@
+
 const { zokou } = require("../framework/zokou");
 const { downloadMediaMessage, downloadContentFromMessage } = require("@whiskeysockets/baileys");
 const { exec } = require('child_process');
